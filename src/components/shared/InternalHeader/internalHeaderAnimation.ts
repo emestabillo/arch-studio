4dd3a25c572b463a341e3625dcdd22aa71fb7e7d
Emmilie Estabillo <emsestabillo@gmail.com>
2024-12-04 23:29:04 -0500

    Adjustments to header animation

--- src/components/shared/InternalHeader/internalHeaderAnimation.ts
+++ src/components/shared/InternalHeader/internalHeaderAnimation.ts
@@ -9,63 +9,61 @@ export const createTimeline = (
   containerRef: MutableRefObject<HTMLDivElement | null>
 ) => {
   if (containerRef.current) {
-    if (containerRef.current) {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: "top 80%",
-          // invalidateOnRefresh: true,
-        },
-      });
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top 80%",
+      },
+    });
 
-      tl.fromTo(
-        containerRef.current?.querySelector("h1"),
+    tl.fromTo(
+      containerRef.current?.querySelector("h1"),
+      {
+        clipPath: "inset(0 100% 0 0)",
+        opacity: 0,
+      },
+      {
+        clipPath: "inset(-2rem 0% 0 0)",
+        opacity: 1,
+        duration: 1,
+        ease: "power2.out",
+        stagger: 0.1,
+      }
+    )
+      .fromTo(
+        containerRef.current?.querySelector(`.${styles.imageContainer}`),
         {
-          clipPath: "inset(0 100% 0 0)",
           opacity: 0,
         },
         {
-          clipPath: "inset(-2rem 0% 0 0)",
           opacity: 1,
+          duration: 1.5,
+          ease: "power2.out",
+        }
+      )
+      .fromTo(
+        containerRef.current?.querySelectorAll("h2, p"),
+        {
+          // "--translateY": 50,
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          // "--translateY": 0,
           duration: 1,
           ease: "power2.out",
           stagger: 0.1,
-        }
+        },
+        "<"
       )
-        .fromTo(
-          containerRef.current?.querySelector(`.${styles.imageContainer}`),
-          {
-            opacity: 0,
-          },
-          {
-            opacity: 1,
-            duration: 1.5,
-            ease: "power2.out",
-          }
-        )
-        .fromTo(
-          containerRef.current?.querySelectorAll("h2, p"),
-          {
-            opacity: 0,
-          },
-          {
-            opacity: 1,
-            "--translateY": 0,
-            duration: 1,
-            ease: "power2.out",
-            stagger: 0.1,
-          },
-          "-=0.85"
-        )
-        .to(
-          containerRef.current?.querySelector(`.${styles.subtext}`),
-          {
-            "--myScale": 1,
-            duration: 1,
-            ease: "power2.out",
-          },
-          "<"
-        );
-    }
+      .to(
+        containerRef.current?.querySelector(`.${styles.subtext}`),
+        {
+          "--myScale": 1,
+          duration: 1,
+          ease: "power2.out",
+        },
+        "-=0.5"
+      );
   }
 };
