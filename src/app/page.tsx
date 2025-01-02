564cbd9481fe7f201c6915f45bec83ddc76e7ba1
Emmilie Estabillo <emsestabillo@gmail.com>
2025-01-02 16:45:24 -0500

    Add fadein to featured projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import arrow from "../../public/icons/icon-arrow.svg";
 import ImageWithOverlayText from "@/components/shared/ImageWithOverlayText/ImageWithOverlayText";
 import imageSrcMobile from "../../public/images/home/mobile/image-small-team.jpg";
 import imageSrcTablet from "../../public/images/home/tablet/image-small-team.jpg";
 import imageSrcDesktop from "../../public/images/home/desktop/image-small-team.jpg";
-import ProjectCard from "@/components/portfolio/ProjectCard";
-import { projectList } from "@/data/portfolio";
-import Button from "@/components/ui/Button/Button";
 import styles from "./home.module.scss";
 import TwoColTextWithImage from "@/components/shared/TwoColTextWithImage/TwoColTextWithImage";
 import welcomeImage from "../../public/images/home/desktop/image-welcome.jpg";
 import Carousel from "@/components/shared/Carousel/Carousel";
+import FeaturedProjects from "@/components/home/FeaturedProjects/FeaturedProjects";
 
 export default function Home() {
   return (
@@ -38,32 +35,7 @@ export default function Home() {
           enableAnimation={true}
         />
       </section>
-      <section className={`container top-spacing ${styles.featuredSection}`}>
-        <h2>Featured</h2>
-        <ul className={styles.featuredCardsContainer}>
-          {projectList
-            .filter((project) => project.featured)
-            .map((project) => (
-              <ProjectCard
-                key={project.projectTitle}
-                variant="numberedCard"
-                projectTitle={project.projectTitle}
-                subHeading={project.subHeading}
-                imageSrcDesktop={project.imageSrcDesktop}
-                imageSrcTablet={project.imageSrcTablet}
-                imageSrcMobile={project.imageSrcMobile}
-                ctaLinkText="View all projects"
-                ctaLinkUrl="/portfolio"
-              />
-            ))}
-        </ul>
-        <Button
-          text="See all"
-          icon={arrow}
-          href="/portfolio"
-          otherClass={styles.featuredButton}
-        />
-      </section>
+      <FeaturedProjects />
     </main>
   );
 }
